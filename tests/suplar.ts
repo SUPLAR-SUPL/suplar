116c47de9b13540b5fc3ca544fe443be6c105e99
Natnael <natnaelgetenew@gmail.com>
2022-02-15 10:08:42 +0300

    simple test for fund

--- tests/suplar.ts
+++ tests/suplar.ts
@@ -1,6 +1,8 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { Suplar } from '../target/types/suplar';
+import * as assert from "assert";
+
 
 describe('suplar', () => {
 
@@ -9,9 +11,36 @@ describe('suplar', () => {
 
   const program = anchor.workspace.Suplar as Program<Suplar>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
-  });
+  // it('Is initialized!', async () => {
+  //   // Add your test here.
+  //   const tx = await program.rpc.initialize({});
+  //   console.log("Your transaction signature", tx);
+  // });
+
+
+it('can create fund', async () => {
+    // Call the "SendTweet" instruction.
+    const fund = anchor.web3.Keypair.generate();
+    await program.rpc.createFund('veganism', 'Hummus, am I right?', {
+        accounts: {
+            fund: fund.publicKey,
+            creator: program.provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [fund],
+    });
+
+    // Fetch the account details of the created tweet.
+    const fundAccount = await program.account.fund.fetch(fund.publicKey);
+  	console.log(fundAccount);
+
+        // Ensure it has the right data.
+    assert.equal(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, 'veganism');
+    assert.equal(tweetAccount.content, 'Hummus, am I right?');
+    assert.ok(tweetAccount.timestamp);
+
+});
+
+
 });
